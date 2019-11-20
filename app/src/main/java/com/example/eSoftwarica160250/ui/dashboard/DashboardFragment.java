5537acdc8c57c3f30e33935e8516cfcf4251b542
spant786 <spant786@gmail.com>
2019-11-20 21:34:51 +0545

    Initial commit

---
+++ app/src/main/java/com/example/eSoftwarica160250/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,35 @@
+package com.example.eSoftwarica160250.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.eSoftwarica160250.R;
+
+public class DashboardFragment extends Fragment {
+
+    WebView webview;
+
+    private DashboardViewModel dashboardViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        dashboardViewModel =
+                ViewModelProviders.of(this).get(DashboardViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        webview = root.findViewById(R.id.webview);
+
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setWebViewClient(new WebViewClient());
+        webview.loadUrl("https://softwarica.edu.np");
+
+        return root;
+    }
+}
